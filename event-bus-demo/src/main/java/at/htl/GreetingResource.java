093cd55b4ea182df8e0af7b043201d38645515b3
Tarik Hosic <tarikhosic@pop-os.localdomain>
2021-11-26 13:07:02 +0100

    added endpoint that does nothing

--- event-bus-demo/src/main/java/at/htl/GreetingResource.java
+++ event-bus-demo/src/main/java/at/htl/GreetingResource.java
@@ -2,6 +2,7 @@ package at.htl;
 
 import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.mutiny.Uni;
+import io.vertx.core.eventbus.Message;
 import org.eclipse.microprofile.context.ManagedExecutor;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -21,5 +22,11 @@ public class GreetingResource {
     @ConsumeEvent("greeting2")
     public Uni<String> process(String name) {
         return Uni.createFrom().item(name::toUpperCase).emitOn(executor);
+    }
+
+    @ConsumeEvent("greeting3")
+    public void doNothing(Message<String> name) {
+        System.out.println(name.address().toUpperCase());
+        System.out.println(name.body().toUpperCase());
     }
 }
