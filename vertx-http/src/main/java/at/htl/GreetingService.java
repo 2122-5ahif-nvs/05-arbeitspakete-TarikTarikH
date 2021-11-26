f2c8a3ae3360851625e1d94ccd1aa2f89f0f08bb
Tarik Hosic <tarikhosic@pop-os.localdomain>
2021-11-26 13:22:08 +0100

    added greeting service

---
+++ vertx-http/src/main/java/at/htl/GreetingService.java
@@ -0,0 +1,13 @@
+package at.htl;
+
+import io.quarkus.vertx.ConsumeEvent;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    @ConsumeEvent("greeting")
+    public String greeting(String name) {
+        return "Hello " + name;
+    }
+}
